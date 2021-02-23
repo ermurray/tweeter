bb66216667534efacd3d8c5e70b63276e0a75fa2
ermurray <ermurray31@gmail.com>
2021-02-23 23:38:43 +0000

    added coposer-char-counter.js to count char entered in form for new tweet. currently counts and live adjust char count based on input have form set to max length of 140 need to change to accept longer and change count to red

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#tweet-text").keyup(function() {
+    let maxChar = 140;
+    $("#counter").text(`${maxChar -= $(this).val().length}`);
+    // $("#counter").toggleClass(function() {
+
+    // });
+    // }
+
+  });
+});
