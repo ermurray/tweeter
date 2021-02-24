9753526c1ffd5e9241a4ca99c5ddac53d58dbac9
ermurray <ermurray31@gmail.com>
2021-02-24 01:01:54 +0000

    added functionality to turn char counter to red when over limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,8 @@ $(document).ready(function() {
   $("#tweet-text").keyup(function() {
     let maxChar = 140;
     $("#counter").text(`${maxChar -= $(this).val().length}`);
-    // $("#counter").toggleClass(function() {
-
-    // });
-    // }
+    $("#counter").toggleClass("exceeded", maxChar < 0);
+  
 
   });
 });
